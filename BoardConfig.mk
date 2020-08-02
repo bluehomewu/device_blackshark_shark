b3dfab72194fddbf08b0914acb1a75dba73d9b74
EdwardWu <56945729+bluehomewu@users.noreply.github.com>
2020-08-02 15:42:35 +0800

    Update : Fix some error

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,7 @@ TARGET_USES_UEFI := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += skip_override androidboot.fastboot=1
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_PREBUILT_KERNEL := kernel/blackshark/sdm845
 TARGET_KERNEL_CONFIG := sdm845_defconfig
